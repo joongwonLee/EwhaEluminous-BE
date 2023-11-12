62e0d217f82acad793a7717b85d67fc35a5737c9
joongwon <wnddnjs823@ajou.ac.kr>
2023-11-12 18:49:15 +0900

    스프링부트 프로젝트 업로드

---
+++ backend/src/main/java/com/eluminous/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.eluminous.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
