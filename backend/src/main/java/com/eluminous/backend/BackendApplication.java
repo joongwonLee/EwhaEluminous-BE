eca1433850feb5c211bd65ccf6a08e2b71a09696
CNH <zetta_byte@naver.com>
2023-11-20 23:27:43 +0900

    CORS

--- backend/src/main/java/com/eluminous/backend/BackendApplication.java
+++ backend/src/main/java/com/eluminous/backend/BackendApplication.java
@@ -2,9 +2,11 @@ package com.eluminous.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@CrossOrigin("*")
 public class BackendApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/eluminous/backend/domain/thesis/controller/ThesisController.java
+++ backend/src/main/java/com/eluminous/backend/domain/thesis/controller/ThesisController.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
@@ -17,6 +14,7 @@ import java.util.List;
 
 @RestController
 @Slf4j
+@CrossOrigin("*")
 public class ThesisController {
 
 
